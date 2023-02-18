742db4fd6b48f801215af2f75672fcf26e729b99
Amogelang <Tshiamomalebye3310@gmail.com>
2023-02-17 18:05:48 -0800

    spaces

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,9 @@
  */
 int main(void)
 {
-printf("The size of int is: %lu.\n", (unsigned long)sizeof(i));
-printf("The size of double is: $lu.\n", (unsigned long)sizeof(d));
-printf("The size of char is: %lu./n", (unsugned long)sizeof(c));
-
-make size
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of an int: %ld byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+printf("Size of a float: %ld byte(s)\n", sizeof(float));
 return (0);
-}
