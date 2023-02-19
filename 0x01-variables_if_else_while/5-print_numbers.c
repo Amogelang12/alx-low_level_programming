3bee4e965b79699f212bba5e1c290a365f9afe91
Amogelang <Tshiamomalebye3310@gmail.com>
2023-02-19 07:13:01 -0800

    zero to one

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * main -start of c program
+ * main - to print all single numbers of base 10
  *
  * Return: 0, if succesful
  */
 int main(void)
 {
-	int num;
-	for (num = 0 ; num < 10 ; num++)
-	printf("%d", num);
+	int A;
+	/*to print all single numbers of base 10,loop through them*/
+	for (A = 0 ; A < 10 ; A++)
+	printf("%d", A);
 	printf("\n");
 	return (0);
 }
