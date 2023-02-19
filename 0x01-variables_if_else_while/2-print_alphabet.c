ed5cd8de754a8b9a9564e7ea6ed39a0207fe4a25
Amogelang <Tshiamomalebye3310@gmail.com>
2023-02-19 04:59:13 -0800

    alpha in c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main -is the entry point for c program
+ *
+ *Return:zero if no error, Non-zero value  if errors
+ */
+
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('n\');
+return (0);
+}
