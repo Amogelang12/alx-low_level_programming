0e2d5f7f53f5c4f126e5e31caf9cd33b3fb8a719
Amogelang <Tshiamomalebye3310@gmail.com>
2023-02-19 06:58:56 -0800

    zero to one

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main -start of c program
+ *
+ * Return: 0, if succesful
+ */
+int main(void)
+{
+	int num;
+	for (num = 0 ; num < 10 ; num++)
+	printf("%d", num);
+	printf("\n");
+	return (0);
+}
