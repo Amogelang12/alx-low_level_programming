bd507ccb4aa465ccb4b42da333a8a4a10f7a2085
Amogelang <Tshiamomalebye3310@gmail.com>
2023-02-19 05:56:29 -0800

    more alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *main -is the entry point for c program
+ *
+ *Return:zero if no error, Non-zero value  if errors
+ */
+
+int main(void)
+{
+char al;
+
+for (al = 'a' ; al <= 'z' ; al++)
+{
+putchar(al);
+}
+for (al = 'A' ; al <= 'Z' ; al++)
+{
+putchar(al);
+}
+putchar('\n');
+return (0);
+}
