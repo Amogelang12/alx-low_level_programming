8ba8feb3635890d34fb7bb8bc6fdbb5ac8c5c7f3
Amogelang12 <124150459+Amogelang12@users.noreply.github.com>
2023-02-20 11:24:48 +0200

    putchar with numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *main -is the entry point for c program
+ *
+ *Return:zero if no error, Non-zero value  if errors
+ */
+
+int main(void)
+{
+int num;
+
+for (num = '0' ; num <= '10' ; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}
