a6d2ae81581599bd27d897c642c272db60cd8074
Amogelang12 <124150459+Amogelang12@users.noreply.github.com>
2023-02-20 11:39:24 +0200

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *main -is the entry point for c program
+ *
+ *Return:zero if no error, Non-zero value  if errors
+ */
+
+int main(void)
+{
+char la;
+
+for (la = 'z' ; al => 'a' ; la++)
+{
+putchar(la);
+}
+putchar('\n');
+return (0);
+}
