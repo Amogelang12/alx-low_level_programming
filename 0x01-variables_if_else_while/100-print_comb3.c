05ce1fe845cf3a566542d5fb8d2bfda033de0bdd
Amogelang12 <124150459+Amogelang12@users.noreply.github.com>
2023-02-21 04:08:33 +0200

    Long combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,14 +8,14 @@ int main(void)
 {
 int num1 , num2;
 
-for (num1 = 0 ; num1 < 8 ; num1++)
+for (num1 = 0 ; num1 <= 8 ; num1++)
 {
-for (num2 = 1 ; num2 < 9 ; num2++)
+for (num2 = 1 ; num2 <= 9 ; num2++)
 {
 putchar((num1 % 8) + '0');
 putchar((num2 % 9) + '1');
-if (num1 == 8 && num2 == 9)
 
+if (num1 == 8 && num2 == 9)
 continue;
 putchar(',');
 putchar(' ');
