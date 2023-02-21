eda363d837ef839506584d08edf0578a6cb989a7
Amogelang12 <124150459+Amogelang12@users.noreply.github.com>
2023-02-21 03:54:08 +0200

    Diff

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+Bytes
+#include <stdio.h>
+/**
+ * main - is entry of program
+ *
+ * Return: 0, if succesful
+ */
+int main(void)
+{
+int num;
+
+for (num = 01 ; x num < 89 ; num++)
+{
+putchar((num % 89) + '01');
+if (num == 88)
+
+continue;
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
