530d92ea7ba5f65d2db494ffaff42dffaf767650
Amogelang12 <124150459+Amogelang12@users.noreply.github.com>
2023-02-21 03:39:48 +0200

    Num and alph

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - is entry of program
+ *
+ * Return: 0, if succesful
+ */
+int main(void)
+{
+int num;
+char al;
+
+for (num = 0 ; num < 10 ; num++)
+putchar((x % 10) + '0');
+
+for (al = 'a' ; al <= 'f' ; al++)
+putchar(al);
+putchar('\n');
+return (0);
+}
