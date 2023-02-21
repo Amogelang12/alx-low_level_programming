f82f9f694bb0fd017317e950f7d3d9205d653c38
Amogelang12 <124150459+Amogelang12@users.noreply.github.com>
2023-02-21 04:50:12 +0200

    Last task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,12 +12,12 @@ for (num1 = 0 ; num1 <= 98 ; num1++)
 {
 for (num2 = num1 + 1 ; num2 <= 99 ; num2++)
 {
-putchar((a / 10) + '0');
+putchar((num1 / 10) + '0');
 			
-putchar((a % 10) + '0');
+putchar((num1 % 10) + '0');
 putchar(' ');
-putchar((b / 10) + '0');
-putchar((b % 10) + '0');
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
 
 if (num1 == 98 && num2 == 99)
 continue;
