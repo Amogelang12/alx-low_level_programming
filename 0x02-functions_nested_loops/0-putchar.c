4fdf9b70c45d0a8bf3e9d2ff69ae1752f0a52cca
Amogelang <Tshiamomalebye3310@gmail.com>
2023-02-22 09:03:24 -0800

    put

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main -is the entry point for c program
+ *
+ *Return:zero if no error, Non-zero value  if errors
+ */
+
+int main(void)
+{
+char name = "_putchar";
+
+printf("%s\n", name);
+return (0);
+}
