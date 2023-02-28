ce821b0c09ac0158476a185ada2c90f49ca263c4
Amogelang <Tshiamomalebye3310@gmail.com>
2023-02-27 16:02:55 -0800

    c coding

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+julien@ubuntu:~/0x05$ cat 0-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code 
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int n)
+{
+*ptr_n =98;
+printf("n=%d\n", n);
+}
