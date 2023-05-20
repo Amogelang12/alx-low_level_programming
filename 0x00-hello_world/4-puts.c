06735636b8b9fa318367f43b00770ed127e06383
Amogelang <tshiamomalebye3310@gmail.com>
2023-05-20 15:24:06 -0700

    print using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main -is the entry point for the program.
+ *
+ * Return:zero if no error, Non-zero value  if errors
+ */
+int main(void)
+{
+char name[100] = "Programming is like building a multilingual puzzle\n";
+
+puts(name);
+return (0);
+}
