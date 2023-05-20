dd510adc3dc4b27637134d16703a13c4380aa0cf
Amogelang <tshiamomalebye3310@gmail.com>
2023-05-20 15:45:42 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
  *
  * Return:zero if no error, Non-zero value  if errors
  */
-int main (void)
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+return (0);
 }
